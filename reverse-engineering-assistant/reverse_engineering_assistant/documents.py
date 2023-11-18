14e3ff6cc1f867182f0f24247824c1f67c192500
カイダ <kaida@cyberkaida.gg>
2023-11-18 19:25:07 +0900

    Add deserialisation for documents

--- reverse-engineering-assistant/reverse_engineering_assistant/documents.py
+++ reverse-engineering-assistant/reverse_engineering_assistant/documents.py
@@ -20,7 +20,7 @@ like a push/pull model).
 
 from __future__ import annotations
 import json
-from typing import Dict, Optional, List, Type
+from typing import Dict, Optional, List, Type, LiteralString
 import logging
 from pathlib import Path
 
@@ -37,7 +37,7 @@ class AssistantDocument(object):
     name: str
     content: str
     metadata: Dict[str, str]
-    document_type: str
+    document_type: LiteralString
 
     def __init__(self,
                  name: str,
@@ -67,6 +67,12 @@ class AssistantDocument(object):
     def from_json(cls, json_str: str) -> AssistantDocument:
         data = json.loads(json_str)
         logger.debug(f"Loading document from json: {json_str}")
+
+        t = document_type_map[data['metadata']['document_type']]
+        if t != AssistantDocument:
+            logger.info(f"Loading document of type {t}")
+            return t.from_json(json_str)
+
         return AssistantDocument(
             name=data['name'],
             content=data['content'],
@@ -77,6 +83,37 @@ class AssistantDocument(object):
 @document_type
 class DecompiledFunctionDocument(AssistantDocument):
     document_type = 'decompiled_function'
+
+    @property
+    def function_start_address(self) -> str:
+        return self.metadata['function_start_address']
+    
+    @property
+    def function_signature(self) -> str:
+        return self.metadata['function']
+    
+    @property
+    def inbound_calls(self) -> List[str]:
+        return self.metadata['inbound_calls']
+    
+    @property
+    def outbound_calls(self) -> List[str]:
+        return self.metadata['outbound_calls']
+    
+    @classmethod
+    def from_json(cls, json_str: str) -> DecompiledFunctionDocument:
+        data = json.loads(json_str)
+        logger.debug(f"Loading decompiled function document from json: {json_str}")
+
+        return DecompiledFunctionDocument(
+            function_name=data['metadata']['function_name'],
+            decompilation=data['content'],
+            function_start_address=data['metadata']['function_start_address'],
+            function_signature=data['metadata']['function_signature'],
+            inbound_calls=data['metadata']['inbound_calls'],
+            outbound_calls=data['metadata']['outbound_calls'],
+        )
+
     def __init__(self,
                  function_name: str,
                  decompilation: str,
@@ -90,17 +127,15 @@ class DecompiledFunctionDocument(AssistantDocument):
         if isinstance(function_start_address, int):
             function_start_address = hex(function_start_address)
 
-        name = function_name
-        content = json.dumps({
+        content = json.dumps(decompilation, indent=2, sort_keys=True)
+        metadata = {
+            'function_name': function_name,
+            'function_start_address': function_start_address,
+            'function_signature': function_signature,
             'inbound_calls': inbound_calls or [],
             'outbound_calls': outbound_calls or [],
-            'decompilation': decompilation,
-        }, indent=2, sort_keys=True)
-        metadata = {
-            'address': function_start_address,
-            'function': function_signature,
         }
-        super().__init__(name=name, content=content, document_type=self.document_type, metadata=metadata)
+        super().__init__(name=function_name, content=content, document_type=self.document_type, metadata=metadata)
 
     def __repr__(self) -> str:
         return f"DecompiledFunctionDocument(name={self.name}, metadata={self.metadata}, content_length={len(self.content)})"
@@ -108,10 +143,35 @@ class DecompiledFunctionDocument(AssistantDocument):
 @document_type
 class CrossReferenceDocument(AssistantDocument):
     document_type = 'cross_reference'
-    subject_address: str
-    references_to: List[str]
-    references_from: List[str]
-    symbol: str
+
+    @property
+    def subject_address(self) -> str:
+        return self.metadata['subject_address']
+    
+    @property
+    def references_to(self) -> List[str]:
+        return self.metadata['references_to']
+    
+    @property
+    def references_from(self) -> List[str]:
+        return self.metadata['references_from']
+    
+    @property
+    def symbol(self) -> str:
+        return self.metadata['symbol']
+    
+    @classmethod
+    def from_json(cls, json_str: str) -> CrossReferenceDocument:
+        data = json.loads(json_str)
+        logger.debug(f"Loading cross reference document from json: {json_str}")
+
+        return CrossReferenceDocument(
+            address=data['metadata']['address'],
+            symbol=data['metadata']['symbol'],
+            references_to=data['metadata']['references_to'],
+            references_from=data['metadata']['references_from'],
+        )
+
     def __init__(self,
                  address: int | str,
                  symbol: str,
@@ -125,28 +185,18 @@ class CrossReferenceDocument(AssistantDocument):
         # First normalise the lists
         references_to = [hex(x) if isinstance(x, int) else x for x in references_to]
         references_from = [hex(x) if isinstance(x, int) else x for x in references_from]
-
-        self.subject_address = address
-        self.references_to = references_to
-        self.references_from = references_from
-        self.symbol = symbol
         
         # The content is a json document of references to and from the given address
-        json_doc = json.dumps({
-            'address': address,
-            'symbol': symbol,
-            'to_this_address': references_to,
-            'from_this_address': references_from,
-        })
         # TODO: This is presentation layer to the model, it belongs in
         # assistant, not in document
-        content = f"""
-        Cross references for {address} in json format:
-        {json_doc}
-        """
+        content = f""
 
         metadata = {
             'address': address,
+            'subject_address': address,
+            'references_to': references_to,
+            'references_from': references_from,
+            'symbol': symbol,
         }
 
         super().__init__(name=name, content=content, document_type=self.document_type, metadata=metadata)
